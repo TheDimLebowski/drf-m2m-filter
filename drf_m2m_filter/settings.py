b742a7b7ea6725ddd5b1761d5804f52520f7cdda
Dimitry <dimitry.foures@gmail.com>
2016-09-07 14:58:23 +0100

    changed to sqlite3

--- drf_m2m_filter/settings.py
+++ drf_m2m_filter/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'drf_m2m_filter.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'test',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
 
 # Password validation
