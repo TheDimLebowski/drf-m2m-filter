f0d9291cacf3050638f77dce98d80cbcc8da5c3c
Dimitry <dimitry.foures@gmail.com>
2016-09-07 14:52:51 +0100

    import fixed

--- test_app/filters.py
+++ test_app/filters.py
@@ -2,8 +2,8 @@ import django_filters
 from models import User, Label
 
 class UserFilter(django_filters.FilterSet):
-    labels = ModelChoiceFilter(queryset=Label.objects.all())
-    
+    labels = django_filters.ModelChoiceFilter(queryset=Label.objects.all())
+
     class Meta:
         model = User
         fields = ('labels',)
